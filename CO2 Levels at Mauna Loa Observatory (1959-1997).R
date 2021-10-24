80482dc1968a25d6ce68b8e2e205e2ed2040dfd0
Jayanth.C <59359153+benaka-tech@users.noreply.github.com>
2021-10-24 11:25:26 +0530

    Delete CO2 Levels at Mauna Loa Observatory (1959-1997).R

--- CO2 Levels at Mauna Loa Observatory (1959-1997).R
+++
@@ -1,4 +0,0 @@
-MaunaLoa = read.csv(file.choose(),header=T)
-names(MaunaLoa)
-co2.ts = ts(MaunaLoa$CO2,start=1959,frequency=12)
-autoplot(co2.ts) + ggtitle("CO2 Levels - Mauna Loa Observatory (1959-1997)") + xlab("Year") + ylab("CO2 Concentration (ppm)")
