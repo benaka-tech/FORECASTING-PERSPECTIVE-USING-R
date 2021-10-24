721c6c09373def8c37ee04ac27f842a5e0cd5a3c
Jayanth.C <59359153+benaka-tech@users.noreply.github.com>
2021-10-24 11:25:33 +0530

    Delete Economy Passenger Load Between Sydney and Melbourne.R

--- Economy Passenger Load Between Sydney and Melbourne.R
+++
@@ -1,7 +0,0 @@
-require(fpp2)
-MelSyd = read.csv(file.choose(),header=T)
-names(MelSyd)
-Econ = MelSyd$Economy.Class
-Econ = ts(Econ,start=c(1987,25),frequency=52)
-Econ
-autoplot(Econ) + ggtitle("Melbourne-Sydney Economy Air Passengers") + xlab("Year") + ylab("Thousands of Passengers")
